72ca7ef97ced05db157ecb53719d911d777423ab
SHREYANCE1 <shreyancejain94@gmail.com>
2019-03-11 20:44:13 +0530

    nginx on heroku with node

--- server.js
+++ server.js
@@ -11,15 +11,9 @@ app.use(express.static(__dirname + '/dist/angular-heroku-app'))
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/angular-heroku-app/index.hmtl'))
+    res.sendFile(path.join(__dirname+'/landingPage/index.html'))
 
 })
 
 //start the app by listening on the default Heroku port
-app.listen((process.env.PORT || 8080), function(){
-    if(process.env.DYNO) {
-        console.log('running on heroku')
-        fs.openSync('/tmp/app-initialized','w')
-    }
-    console.log('node app is runnning on port: can we get the port');
-});
+app.listen(process.env.PORT || 8080);
